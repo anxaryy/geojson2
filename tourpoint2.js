516a3ad2996859c4e92c21367e00af06a77444ef
anxaryy <124546128+anxaryy@users.noreply.github.com>
2023-04-24 11:31:51 +0700

    Add files via upload

---
+++ tourpoint2.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ห้างไชยแสง",
+          "type": "special",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.3892954185165,
+            14.887646245250124
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดนัดคลองถม",
+          "type": "special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.39320766757578,
+            14.896671475748931
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "พิพิธภัณฑ์",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.33262290381697,
+            14.997659394425071
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "tops plaza",
+          "type": "special",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.40799275951889,
+            14.90540873428256
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดไทร",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.29486435476764,
+            15.064828085994549
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดโบสถ์",
+          "type": "culture",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.33420516867596,
+            14.993286270109223
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดย้อนยุคบางระจัน",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.32102134352584,
+            14.800772977781108
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดโพธิ์เก้าต้น",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.32103974612528,
+            14.801466496572615
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดพิกุลทอง",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.40370107484057,
+            14.782535128917118
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อนุสาวรีย์บางระจัน",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.32072154942409,
+            14.805445343929307
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
